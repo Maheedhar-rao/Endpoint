d87f3fc903697fe3bcb0a7a64b692051e39c4fb1
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-12-09 22:19:45 -0500

    Update app.py
    changed the port number from 5001 to 8080

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def fetch_pdf(token: str):
         abort(500)
         
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5001))
+     port = int(os.environ.get("PORT", 8080))
      app.run(host="0.0.0.0", port=port, debug=False)
